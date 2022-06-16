6338b3a193911ac5f2ff6e78ce68de3dea03d2f7
JongDuk-Hahm <neo.jd@neolab.net>
2022-06-16 14:14:39 +0900

    Update MainMenu.java
    메인메뉴의 할당값 변경

--- NeoStudio/src/Base/MainMenu.java
+++ NeoStudio/src/Base/MainMenu.java
@@ -19,18 +19,25 @@ public class MainMenu {
     public static void MainMenu() {
         try {
             Thread.sleep(2000);
+            
+            
+            // Timeline 클리 후 페이지다운키 누르기
+            WebElement categoryTime = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[1]/div/div[2]/li[1]"));
+            Actions bulider = new Actions(driver);
+           bulider.click(categoryTime)
+                    .sendKeys(Keys.chord(Keys.PAGE_DOWN)).perform();
 
-            WebElement categoryTime = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div[1]/div/div[2]/li[1]/span"));
-            WebElement scrolldown = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div[1]/div/div[2]/li[6]/span"));
-            // setting xpath 위치로 스크롤 다운 실행
-            Actions builder = new Actions(driver);
-//            builder.clickAndHold(categoryTime).moveToElement(scrolldown).perform();
 
-            builder.click(categoryTime).sendKeys(Keys.chord(Keys.PAGE_DOWN));
-            // click 하고 홀드된 상태에서 셋팅 위치까지 스크롤 다운
+            // mainMenuName 어레이 리스트에 각 메뉴의 엘리먼트 할당하기
+            WebElement temp;
+            ArrayList<WebElement> mainMenuName = new ArrayList<WebElement>(); //메인메뉴 webelement 담는 배열
+            for(int i = 1; i<7; i++){
+                temp = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[1]/div/div[2]/li["+i+"]"));
+                mainMenuName.add(temp);
+            }
 
-            ArrayList<WebElement> mainMenuName =new ArrayList<WebElement>(); //메인메뉴 webelement 담는 배열
-            mainMenuName = (ArrayList<WebElement>) driver.findElements(By.cssSelector("li.MuiButtonBase-root.MuiListItem-root.MuiMenuItem-root.jss43.MuiMenuItem-gutters.MuiListItem-gutters.MuiListItem-button"));
+            // Test code
+//            System.out.println(mainMenuName.size());
 
             Timeline = mainMenuName.get(0);
             Notebook= mainMenuName.get(1);
@@ -38,8 +45,7 @@ public class MainMenu {
             Record= mainMenuName.get(3);
             Favorite= mainMenuName.get(4);
             Setting= mainMenuName.get(5);
-
-
+//
         }  catch (Exception e) {
             e.printStackTrace();
         }
