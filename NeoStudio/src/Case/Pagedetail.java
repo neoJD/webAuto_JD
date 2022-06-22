a0a588386bbdb734d0a7801f18bc3b17e26cd862
Hansol Kim <hansol@neolab.net>
2022-06-22 19:55:28 +0900

    Update Pagedetail.java
    파일 위치 변경 부탁드립니다.

--- NeoStudio/src/Case/Pagedetail.java
+++ NeoStudio/src/Case/Pagedetail.java
@@ -2,7 +2,6 @@ package Case;
 import Base.MainMenu;
 
 import Base.PageViewItem;
-import Base.RunTime;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -22,11 +21,9 @@ import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 
 public class Pagedetail{
 
-    // 선언할거 정해주기
     public static String PageTotal;
     public static String PageTotalStr;
     public static WebElement Notebook;
@@ -35,28 +32,24 @@ public class Pagedetail{
     public static int testcount=3;
 
 
-    public static void Detail() throws InterruptedException {
+    public static void PageDetail() throws InterruptedException {
 
         MainMenu M = new MainMenu();
         M.MainMenu();
         MainMenu.Notebook.click();
         Thread.sleep(7000);
 
-        // 첫번째 notebook  클릭
         Notebook = driver.findElement(By.cssSelector("ul > div:nth-child(1) > button > div > img"));
         Notebook.click();
 
         Thread.sleep(7000);
-
+        //노트북 페이지 첫페이지 선택
         Pagedetail = driver.findElement(By.cssSelector("ul > div:nth-child(1) > button > div"));
         Pagedetail.click();
-        System.out.println("==== Pagedetail 진입 완료====");
-        // 상세페이지 진입 완료
-        //1. 하단 전체 페이지 수 확인 - 전체 페이지 수 리스트 str->int 전환
+        System.out.println("****************** Pagedetail 진입 완료 ******************");
+
     }
 
-
-    //public class Move{
     public static void FileMove()throws IOException {
 
         File test = new File("/Users/gimhansol/Downloads");
@@ -64,18 +57,17 @@ public class Pagedetail{
 
         File[] fileList = test.listFiles((dir, name) -> name.endsWith("png"));
         // 특정 파일 경로에서 특정 파일명 검색 하여 송출
-        //RunTime.runtime =new RunTime();
-        //setStartTime.setStartTime
 
         ArrayList<String> getFileList = new ArrayList<>();
         //여기부터 file 옮기는 부분
 
-        File dir =new File("/Users/gimhansol/Documents/AutomationProject/0622");
-            //디렉토리 생성
-        boolean DirectoryCreated= dir.mkdir();
-            //결과 출력
-        System.out.println(DirectoryCreated);
-        
+
+        File dir =new File("/Users/gimhansol/Documents/AutomationProject/0623");
+        //디렉토리 생성
+        boolean DirectoryCreated = dir.mkdir();
+
+
+
         for (File file : fileList) {
             getFileList.add(file.toString());//받은 파일 리스트의 원래 경로 저장
         }
@@ -84,16 +76,12 @@ public class Pagedetail{
                 File file = FileUtils.getFile(getFileList.get(i)); //원래 경로
                 fileNameList.add(file.toString().substring(getFileList.get(i).lastIndexOf("/")));//받은 파일 리스트에서 파일명만 저장
 
-//                        File fileToMove = FileUtils.getFile("/Users/gimhansol/Documents/AutomationProject/0622" + fileNameList.get(i));
-//                        //옮기고 싶은 경로 생성 및 파일명 합쳐서 옮길 것
-//                        FileUtils.moveFile(file, fileToMove); //원래 경로에서 옮겨갈 경로로 옮기는 부분
 
                 Path file1 = Paths.get(getFileList.get(i));
-                Path newFile1 = Paths.get("/Users/gimhansol/Documents/AutomationProject/0622" + fileNameList.get(i));
+                Path newFile1 = Paths.get("/Users/gimhansol/Documents/AutomationProject/0623" + fileNameList.get(i));
 
                 Path newFilePath= Files.move(file1, newFile1, StandardCopyOption.REPLACE_EXISTING);
-                System.out.println(newFilePath);
-                
+
             }
 
         } catch (IOException e) {
@@ -101,7 +89,8 @@ public class Pagedetail{
         }
 
     }
-    
+
+
     public static void imagedown() {
         try {
             PageCount = driver.findElement(By.cssSelector("div > span:nth-child(4)"));
@@ -112,72 +101,65 @@ public class Pagedetail{
 
             //저장 누르고 -> 페이지 이동 누르고 -> PageTotalStr만큼
             //for (int i = 1; i <= PageTotalInt; ) {
-            //testcount=3
-            for (int i=1; i<=testcount;){ // 위에꺼로 바꿔주기
+            //testcount = 3;
+            for (int i=1; i<=testcount; i++){ // 위에꺼로 바꿔주기
+                Thread.sleep(5000);
                 PageViewItem P = new PageViewItem();
                 P.Getpageviewitem();
-                Thread.sleep(5000);
-                //System.out.println(i);
                 PageViewItem.ImageDown_Button.click();
-                //PageViewItem.Next_Button.click();
-                i++;
-                for(int j=1; j<testcount-1;) { //코드 손좀 보기
-                    Thread.sleep(2000);
-                    PageViewItem.Next_Button.click();
-                    j++;
+
+
+                try {
+                    LinkPaste_Button.click();
+                    Thread.sleep(4000);
+
+                    WebElement linkPaste_alert = driver.findElement(By.className("MuiAlert-message"));
+                    if(Objects.equals(linkPaste_alert.getText(),"링크가 복사되었습니다."))
+                    {
+                        item.add("링크 복사 토스트 팝업 확인");
+                        result.add("PASS");
+                    }
+                    else{
+                        item.add("링크 복사 토스트 팝업 확인");
+                        result.add("FAIL");
+                    }
                 }
 
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                } finally {
+                }
+
+                for(int j=1; j<testcount-1; j++) {
+                    Thread.sleep(7000);
+                    PageViewItem.Next_Button.click();
+                }
             }
+            Thread.sleep(3000);
+            Back_Button.click();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-    
+
+
     public static void Compare(){
-        File test=new File("/Users/gimhansol/Downloads");
-        File newtest =new File("/Users/gimhansol/Documents/AutomationProject/0622");
+        File Test=new File("/Users/gimhansol/Downloads");
+        File Newtest =new File("/Users/gimhansol/Documents/AutomationProject/0623");
 
 
-        if(test==newtest) {
+        if(Test==Newtest) {
             item.add("기존 파일 이동 파일 일치");
             result.add("PASS");
-            System.out.println("끝났다");
         }
         else{
-                item.add("기존 파일 이동 파일 불일치");
-                result.add("FALSE");
+            item.add("기존 파일 이동 파일 불일치");
+            result.add("FALSE");
         }
 
-
-    }
-    public static void linkPaste(){
-        try {
-            PageViewItem P = new PageViewItem();
-            P.Getpageviewitem();
-            LinkPaste_Button.click();
-            Thread.sleep(10000);
-
-            WebElement linkPaste_alert = driver.findElement(By.className("MuiAlert-message"));
-            if(Objects.equals(linkPaste_alert.getText(),"링크가 복사되었습니다."))
-            {
-                item.add("링크 복사 토스트 팝업 확인");
-                result.add("PASS");
-            }
-            else{
-                item.add("링크 복사 토스트 팝업 확인");
-                result.add("FAIL");
-            }
-        }
-
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }finally {
-        }
 
     }
 
 
 }
-
