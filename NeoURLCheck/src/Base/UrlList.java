80d18ccfbb495bbc86c285f7702809d8d8829041
sykim0116 <103556140+sykim0116@users.noreply.github.com>
2022-06-30 15:29:40 +0900

    Add files via upload

---
+++ NeoURLCheck/src/Base/UrlList.java
@@ -0,0 +1,39 @@
+package Base;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class UrlList {
+    public static ArrayList<String> error_urlList = new ArrayList<>();
+    public static ArrayList<String> error_code = new ArrayList<>();
+    public static ArrayList<String> error_summary = new ArrayList<>();
+    public static ArrayList<String> urlList = new ArrayList<String>
+            (Arrays.asList(
+                    "https://neolab.net",
+                    "https://neolab.kr",
+                    "https://neolab.net",
+                    "https://neolab.co.jp",
+                    "https://neosmartpen.com",
+                    "https://neosmartpen.co.kr",
+                    "https://neosmartpen.jp",
+                    "https://www.neostudio.io/termofuse/ja/",
+                    "https://neosmartpen.de",
+                    "https://neosmartpen.io",
+                    "https://neosmartpen.us",
+                    "https://neolab.net/terms_penmanager_eng/",
+                    "https://neolab.net/terms_penmanager/",
+                    "https://neolab.net/privacy_penmanager/",
+                    "https://www.neolab.net/eula_penmanager_eng/",
+                    "https://www.neolab.net/kr/eula_penmanager/",
+                    "https://neolab.net/opensource_penmanager_windows/",
+                    "https://neolab.net/opensource_penmanager_mac/",
+                    "https://www.neostudio.io/policy/en/",
+                    "https://www.neostudio.io/termofuse/en/",
+                    "https://www.neostudio.io/policy/ko/",
+                    "https://www.neostudio.io/termofuse/ko/",
+                    "https://www.neostudio.io/policy/ja",
+                    "https://www.neostudio.io/termofuse/ja/",
+                    "https://www.neostudio.io/policy/de",
+                    "https://www.neostudio.io/termofuse/de/"
+                    ));
+}
