1c78a761df1c3577b66179b31319e8425f6e2cf5
sykim0116 <103556140+sykim0116@users.noreply.github.com>
2022-06-30 16:00:48 +0900

    Update Main.java

--- NeoURLCheck/src/Main/Main.java
+++ NeoURLCheck/src/Main/Main.java
@@ -1,16 +1,22 @@
 package Main;
 
 import Base.*;
+
 import static Base.CheckURL.*;
+import static Base.UrlList.*;
 
 public class Main {
     public static void main(String[] args) {
         try {
             CheckURL.urlCheck_run();
-            HtmlJavaSend.sendEmail();
-        }catch (Exception e){
+            if (error_urlList.size() > 0) {
+                HtmlJavaSend.sendEmail();
+            } else {
+
+            }
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             driver.close();
         }
     }
